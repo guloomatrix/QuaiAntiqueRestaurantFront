3d2449b9d7ee98663dc76a37cbb3c4f807acb1fc
guloomatrix <melleyanik@hotmail.com>
2024-12-25 22:02:21 +0100

    home galerie signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitname

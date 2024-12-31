c23f8154b29f6d268ef3ff26e64c610b27bd4068
guloomatrix <melleyanik@hotmail.com>
2024-12-31 16:32:53 +0100

    front

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,12 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement mon mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/auth/reservations/allResa.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitname
